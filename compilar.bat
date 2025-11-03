facba536647576e08f73c7708ed9b86f35b756e8
João <joaoguilherme.obf@gmail.com>
2025-11-03 11:39:35 -0300

    Mudar comando para compilar jogo

--- compilar.bat
+++ compilar.bat
@@ -1,1 +1,1 @@
-gcc main.c -o jogo.exe -I C:\raylib\include -L C:\raylib\lib -lraylib -lopengl32 -lgdi32 -lwinmm -static
+gcc src/main.c src/menu/menu.c src/player/player.c src/mapa/mapa_fases.c src/mapa/fases/fase1.c -o jogo.exe -I src -I C:\raylib\include -L C:\raylib\lib -lraylib -lopengl32 -lgdi32 -lwinmm -static
